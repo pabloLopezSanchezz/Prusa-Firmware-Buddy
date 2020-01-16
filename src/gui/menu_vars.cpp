70a08d0eb6d00d9a362d6700e6d656da8b2a03d6
Tomas Kucera <tomik@sazava.eu>
2020-01-16 10:20:52 +0100

    fix bad spelling

--- src/gui/menu_vars.cpp
+++ src/gui/menu_vars.cpp
@@ -94,18 +94,12 @@ constexpr const char Y_home_gcode[] = {
     nth_char(Y_home, 8)
 };
 }
-#ifdef PRINTER_PRUSA_MK4
-constexpr const int32_t filament_change_slow_load_lenght = 30;
-constexpr const int32_t filament_change_fast_load_lenght = 60;
-constexpr const int32_t filament_change_slow_purge_lenght = 20;
-constexpr const float filament_unload_mini_lengt = 80.0F;
-#else
-constexpr const int32_t filament_change_slow_load_lenght = FILAMENT_CHANGE_SLOW_LOAD_LENGTH;
-constexpr const int32_t filament_change_fast_load_lenght = FILAMENT_CHANGE_FAST_LOAD_LENGTH;
-constexpr const int32_t filament_change_slow_purge_lenght = 40;
-constexpr const float filament_unload_mini_lengt = 392.0F;
-#endif
 
-constexpr const int32_t filament_change_full_load_lenght = filament_change_fast_load_lenght + filament_change_slow_load_lenght;
-constexpr const int32_t filament_change_full_purge_load_lenght = filament_change_full_load_lenght + filament_change_slow_purge_lenght;
+constexpr const int32_t filament_change_slow_load_length = FILAMENT_CHANGE_SLOW_LOAD_LENGTH;
+constexpr const int32_t filament_change_fast_load_length = FILAMENT_CHANGE_FAST_LOAD_LENGTH;
+constexpr const int32_t filament_change_slow_purge_length = 40;
+constexpr const float filament_unload_mini_length = 392.0F;
+
+constexpr const int32_t filament_change_full_load_length = filament_change_fast_load_length + filament_change_slow_load_length;
+constexpr const int32_t filament_change_full_purge_load_length = filament_change_full_load_length + filament_change_slow_purge_length;
 
