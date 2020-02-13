23f36c3730de156c047f04a874bf4723ff755a6d
Alan Dragomirecký <alan@dragomirecky.com>
2020-02-13 14:37:28 +0100

    improve uart handler and add support for dynamic typing

--- utils/metrics/collect.py
+++ utils/metrics/collect.py
@@ -16,10 +16,11 @@ class MetricError(Exception):
 
 
 class Point:
-    def __init__(self, timestamp, metric_name, value):
+    def __init__(self, timestamp, metric_name, value, tags):
         self.timestamp = timestamp
         self.metric_name = metric_name
         self.value = value
+        self.tags = tags
 
     @property
     def is_error(self):
@@ -28,7 +29,7 @@ class Point:
 
 class LineProtocolParser:
     def __init__(self):
-        self.last_timestamp = None
+        self.reset_session()
 
     def timestamp_diff_to_timestamp(self, timestamp_diff):
         if self.last_timestamp is None:
@@ -37,27 +38,42 @@ class LineProtocolParser:
             self.last_timestamp += timedelta(milliseconds=timestamp_diff)
         return self.last_timestamp
 
+    def reset_session(self):
+        self.last_timestamp = None
+
+    def parse_value(self, value, flag, tags=None):
+        tags = tags or dict()
+
+        if '&' in value:
+            value, *tags_raw = value.split('&')
+            tags = {t.split('=')[0]: t.split('=')[1] for t in tags_raw}
+            print(value, tags_raw, tags)
+        else:
+            value, tags = value, (tags or dict())
+
+        if flag == 's' and value.startswith('!'):
+            return self.parse_value(value[2:], flag=value[1], tags=tags)
+        elif flag == 'i':
+            return int(value), tags
+        elif flag == 'f':
+            return float(value), tags
+        elif flag == 's':
+            return str(value), tags
+        elif flag == 'e':
+            return MetricError(str(value)), tags
+        else:
+            print('invalid value:', value)
+            return None, None
+
     def parse_line(self, line):
         match = line_re.fullmatch(line)
         if not match:
             return
         timestamp_diff, metric_name, flag, value = match.group(1), match.group(
             2), match.group(3), match.group(4)
-        if flag == 'i':
-            value = int(value)
-        elif flag == 'f':
-            value = float(value)
-        elif flag == 's':
-            value = str(value)
-        elif flag == 'e':
-            value = ''
-        elif flag == 'e':
-            value = MetricError(str(value))
-        else:
-            print('invalid line:', line)
-            return
+        value, tags = self.parse_value(value, flag)
         timestamp = self.timestamp_diff_to_timestamp(int(timestamp_diff))
-        return Point(timestamp, metric_name, value)
+        return Point(timestamp, metric_name, value, tags=tags)
 
 
 class Application:
@@ -76,29 +92,43 @@ class Application:
         }
 
     async def process_task(self):
+        points = []
+        last_sent = asyncio.get_event_loop().time()
         while True:
             try:
                 line = (await self.line_queue.get()).strip()
                 point = self.parser.parse_line(line)
+
                 if point and not point.is_error:
                     influx_point = {
                         'time': point.timestamp,
                         'measurement': point.metric_name,
-                        'tags': self.tags,
+                        'tags': dict(**self.tags, **point.tags),
                         'fields': {
                             'value': point.value
                         },
                     }
-                    await self.influx.write(influx_point)
+                    points.append(influx_point)
                 elif point and point.is_error:
                     print('error:', point)
+
+                if asyncio.get_event_loop().time() - last_sent > 1:
+                    print('writing', len(points), 'points')
+                    await self.influx.write(points)
+                    points = []
+                    last_sent = asyncio.get_event_loop().time()
             except Exception as e:
                 print('error: %s' % e)
+                points = []
+                last_sent = asyncio.get_event_loop().time()
 
     async def readline_task(self):
         while True:
-            line = (await self.serial.readline_async()).decode('ascii')
-            await self.line_queue.put(line)
+            try:
+                line = (await self.serial.readline_async()).decode('ascii')
+                await self.line_queue.put(line)
+            except UnicodeDecodeError:
+                pass
 
     async def run(self):
         await asyncio.gather(self.process_task(), self.readline_task())
