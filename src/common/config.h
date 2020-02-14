ddd6106f8de4645b56d9165e05a3fc2c13f41023
Marek Bel <marek.bel@posta.cz>
2020-02-14 20:24:55 +0100

    Move PRINTER_PRUSA_* macros to include/printers.h

--- src/common/config.h
+++ src/common/config.h
@@ -2,20 +2,7 @@
 #ifndef _CONFIG_H
 #define _CONFIG_H
 
-//--------------------------------------
-//printer variants
-#define PRINTER_PRUSA_MK3 1 //MK3 printer
-#define PRINTER_PRUSA_MINI 2 //MINI printer
-#define PRINTER_PRUSA_XL 3 //XL printer
-#define PRINTER_PRUSA_MK4 4 //MK3 printer with new extruder
-#define PRINTER_PRUSA_IXL 5 //iXL printer with new extruder
-#define PRINTER_PRUSA_MANIPULATOR 6
-#define PRINTER_PRUSA_PICKER 7
-#define PRINTER_PRUSA_EXTRACTOR 8
-
-#ifndef PRINTER_TYPE
-    #error "macro PRINTER_TYPE not defined"
-#endif
+#include "printers.h"
 
 #include <avr/pgmspace.h>
 
