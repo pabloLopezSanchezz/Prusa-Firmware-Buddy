19d212ad193051cf1a1e9c31042c46405555d5a1
Marek Bel <marek.bel@posta.cz>
2020-02-18 17:47:51 +0100

    Split hwio_pindef.h to variant files for each printer.

--- src/common/hwio_pindef.h
+++ src/common/hwio_pindef.h
@@ -1,62 +1,26 @@
-//----------------------------------------------------------------------------//
-// pin names
-#ifndef _HWIO_PINDEF_H
-#define _HWIO_PINDEF_H
+#pragma once
+#include "printers.h"
 
-#define PIN_BEEPER         PA0
-#define PIN_HW_IDENTIFY    PA3
-#define PIN_TEMP_BED       PA4
-#define PIN_THERM2         PA5
-#define PIN_TEMP_HEATBREAK PA6
-#define PIN_Z_MIN          PA8
-
-#define PIN_HEATER_BED PB0
-#define PIN_HEATER_0   PB1
-
-#define PIN_TEMP_0 PC0
-
-#define PIN_X_DIR    PD0
-#define PIN_X_STEP   PD1
-#define PIN_X_ENABLE PD3
-#define PIN_X_DIAG   PE2
-
-#define PIN_Y_DIR    PD12
-#define PIN_Y_STEP   PD13
-#define PIN_Y_ENABLE PD14
-#define PIN_Y_DIAG   PE1
-
-#ifdef BUDDY_XL_ELECTRONICS
-
-    #define PIN_Z_DIR    PD8
-    #define PIN_Z_STEP   PD9
-    #define PIN_Z_ENABLE PD10
-    #define PIN_Z_DIAG   PA15
-
-    #define PIN_E_DIR    PD15
-    #define PIN_E_STEP   PD4
-    #define PIN_E_ENABLE PD2
-    #define PIN_E_DIAG   PE3
-
-#else
-
-    #define PIN_Z_DIR    PD15
-    #define PIN_Z_STEP   PD4
-    #define PIN_Z_ENABLE PD2
-    #define PIN_Z_DIAG   PE3
-
-    #define PIN_E_DIR    PD8
-    #define PIN_E_STEP   PD9
-    #define PIN_E_ENABLE PD10
-    #define PIN_E_DIAG   PA15
-
+#if (!defined(PRINTER_PRUSA_MINI) || !defined(PRINTER_PRUSA_MK4)             \
+    || !defined(PRINTER_PRUSA_XL) || !defined(PRINTER_PRUSA_IXL)             \
+    || !defined(PRINTER_PRUSA_MANIPULATOR) || !defined(PRINTER_PRUSA_PICKER) \
+    || !defined(PRINTER_PRUSA_EXTRACTOR))
+    #error "Some printer type not defined."
 #endif
-
-#define PIN_FAN1    PE9
-#define PIN_FAN     PE11
-#define PIN_BTN_ENC PE12
-#define PIN_BTN_EN1 PE13
-#define PIN_BTN_EN2 PE15
-
-#define PIN_FSENSOR PB4
-
-#endif //_HWIO_PINDEF_H
+#if (PRINTER_TYPE == PRINTER_PRUSA_MINI)
+    #include "hwio_pindef_MINI.h"
+#elif (PRINTER_TYPE == PRINTER_PRUSA_MK4)
+    #include "hwio_pindef_MK4.h"
+#elif (PRINTER_TYPE == PRINTER_PRUSA_XL)
+    #include "hwio_pindef_XL.h"
+#elif (PRINTER_TYPE == PRINTER_PRUSA_IXL)
+    #include "hwio_pindef_IXL.h"
+#elif (PRINTER_TYPE == PRINTER_PRUSA_MANIPULATOR)
+    #include "hwio_pindef_MANIPULATOR.h"
+#elif (PRINTER_TYPE == PRINTER_PRUSA_PICKER)
+    #include "hwio_pindef_PICKER.h"
+#elif (PRINTER_TYPE == PRINTER_PRUSA_EXTRACTOR)
+    #include "hwio_pindef_EXTRACTOR.h"
+#else
+    #error "Unknown PRINTER_TYPE!"
+#endif
