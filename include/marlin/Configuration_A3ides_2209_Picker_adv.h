9dd828254bebde0a0fb42defdfc740ee8b0a06d7
Marek Bel <marek.bel@posta.cz>
2020-02-26 14:16:58 +0100

    Increase picker XY current to 700 mA RMS.

--- include/marlin/Configuration_A3ides_2209_Picker_adv.h
+++ include/marlin/Configuration_A3ides_2209_Picker_adv.h
@@ -1470,7 +1470,7 @@
     #define INTERPOLATE true // Interpolate X/Y/Z_MICROSTEPS to 256
 
     #if AXIS_IS_TMC(X)
-        #define X_CURRENT 350 // (mA) RMS current. Multiply by 1.414 for peak current.
+        #define X_CURRENT 700 // (mA) RMS current. Multiply by 1.414 for peak current.
         #define X_MICROSTEPS 16 // 0..256
         #define X_RSENSE 0.22
     #endif
@@ -1482,7 +1482,7 @@
     #endif
 
     #if AXIS_IS_TMC(Y)
-        #define Y_CURRENT 350
+        #define Y_CURRENT 700
         #define Y_MICROSTEPS 16
         #define Y_RSENSE 0.22
     #endif
