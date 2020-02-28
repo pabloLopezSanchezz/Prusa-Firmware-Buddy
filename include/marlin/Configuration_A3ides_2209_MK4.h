de841a7940aa67d25fefc5fbd18488661f1c4880
Alan Dragomirecký <alan@dragomirecky.com>
2020-02-28 14:07:25 +0100

    mbl: change probing speed

--- include/marlin/Configuration_A3ides_2209_MK4.h
+++ include/marlin/Configuration_A3ides_2209_MK4.h
@@ -893,7 +893,7 @@
 #define Z_PROBE_SPEED_FAST 6 * 100
 
 // Feedrate (mm/m) for the "accurate" probe of each point
-#define Z_PROBE_SPEED_SLOW (Z_PROBE_SPEED_FAST / 5)
+#define Z_PROBE_SPEED_SLOW 70
 
 // The number of probes to perform at each point.
 //   Set to 2 for a fast/slow probe, using the second probe result.
