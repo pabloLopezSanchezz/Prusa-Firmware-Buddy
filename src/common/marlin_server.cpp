bcf43b9bb11cf1e081e94b8f899f0c6db613ccb3
Michal Rudolf <michal.rudolf23@gmail.com>
2020-03-18 12:02:57 +0100

    enable HTTP_CLIENT and refactor device_state change

--- src/common/marlin_server.cpp
+++ src/common/marlin_server.cpp
@@ -71,12 +71,12 @@ typedef struct _marlin_server_t {
     uint8_t pqueue_tail;                                     // copy of planner.block_buffer_tail
     uint8_t pqueue;                                          // calculated number of records in planner queue
     uint8_t gqueue;                                          // copy of queue.length - number of commands in gcode queue
+    uint8_t server_device_state;                             // device state stored by server
     uint32_t command;                                        // actually running command
     uint32_t command_begin;                                  // variable for notification
     uint32_t command_end;                                    // variable for notification
     marlin_mesh_t mesh;                                      // meshbed leveling
     uint64_t mesh_point_notsent[MARLIN_MAX_CLIENTS];         // mesh point mask (points that are not sent)
-    uint8_t device_state_change;                             // marks that change in device's printing state has occured
 } marlin_server_t;
 
 #pragma pack(pop)
@@ -158,6 +158,7 @@ void _server_update_pqueue(void);
 uint64_t _server_update_vars(uint64_t force_update_msk);
 int _process_server_request(char *request);
 int _server_set_var(char *name_val_str);
+void _device_state_change(uint8_t new_state);
 
 //-----------------------------------------------------------------------------
 // server side functions
@@ -175,7 +176,7 @@ void marlin_server_init(void) {
     marlin_server.mesh.xc = 4;
     marlin_server.mesh.yc = 4;
     marlin_server.gcode_name[0] = '\0';
-    marlin_server.device_state_change = 1;
+    _device_state_change(DEVICE_STATE_IDLE);
 }
 
 void print_fan_spd() {
@@ -391,8 +392,7 @@ void marlin_server_manage_heater(void) {
 }
 
 void marlin_server_quick_stop(void) {
-    marlin_server.vars.device_state = DEVICE_STATE_IDLE;
-    marlin_server.device_state_change = 1;
+    _device_state_change(DEVICE_STATE_IDLE);
     planner.quick_stop();
 }
 
@@ -402,8 +402,7 @@ void marlin_server_print_abort(void) {
     print_job_timer.stop();
     queue.clear();
     if(marlin_server.vars.device_state != DEVICE_STATE_ERROR){
-        marlin_server.vars.device_state = DEVICE_STATE_IDLE;
-        marlin_server.device_state_change = 1;
+        _device_state_change(DEVICE_STATE_IDLE);
     }
     //	planner.quick_stop();
     //	marlin_server_park_head();
@@ -415,16 +414,14 @@ void marlin_server_print_pause(void) {
     card.pauseSDPrint();
     print_job_timer.pause();
     queue.inject_P("M125");
-    marlin_server.vars.device_state = DEVICE_STATE_PAUSED;
-    marlin_server.device_state_change = 1;
+    _device_state_change(DEVICE_STATE_PAUSED);
 }
 
 void marlin_server_print_resume(void) {
     wait_for_user = false;
     host_prompt_button_clicked = HOST_PROMPT_BTN_Continue;
     queue.inject_P("M24");
-    marlin_server.vars.device_state = DEVICE_STATE_PRINTING;
-    marlin_server.device_state_change = 1;
+    _device_state_change(DEVICE_STATE_PRINTING);
 }
 
 void marlin_server_park_head(void) {
@@ -770,20 +767,18 @@ uint64_t _server_update_vars(uint64_t update) {
     }
 
     if (update & MARLIN_VAR_MSK(MARLIN_VAR_DEVICE_STATE)) {
-        v.ui8 = marlin_server.vars.device_state;
+        v.ui8 = marlin_server.server_device_state;
         if(v.ui8 == DEVICE_STATE_PRINTING){
             if(!(marlin_server.vars.sd_printing) && marlin_server.command != MARLIN_CMD_M600 && !(marlin_server.vars.motion ? 1 : 0)){
-                marlin_server.device_state_change = 1;
-                marlin_server.vars.device_state = DEVICE_STATE_FINISHED;
+                marlin_server.server_device_state = DEVICE_STATE_FINISHED;
                 // TODO: HARVERST STATE AND THEN BACK TO IDLE
             }
         } else if ((v.ui8 == DEVICE_STATE_IDLE || v.ui8 == DEVICE_STATE_FINISHED) && marlin_server.vars.sd_printing){
-            marlin_server.device_state_change = 1;
-            marlin_server.vars.device_state = DEVICE_STATE_PRINTING;
+            marlin_server.server_device_state = DEVICE_STATE_PRINTING;
         }
-        if(marlin_server.device_state_change){
+        if(marlin_server.server_device_state != marlin_server.vars.device_state){
+            marlin_server.vars.device_state = marlin_server.server_device_state;
             changes |= MARLIN_VAR_MSK(MARLIN_VAR_DEVICE_STATE);
-            marlin_server.device_state_change = 0;
         }
     }
 
@@ -932,6 +927,11 @@ int _server_set_var(char *name_val_str) {
     return 1;
 }
 
+void _device_state_change(uint8_t new_state){
+    marlin_server.server_device_state = new_state;
+    marlin_server_update(MARLIN_VAR_MSK(MARLIN_VAR_DEVICE_STATE));
+}
+
 // this is extern from guimain.c, used in temporary fix (force_M600_notify)
 // this variable is set imediately after
 extern int gui_marlin_client_id;
@@ -1030,8 +1030,7 @@ void onPrinterKilled(PGM_P const msg, PGM_P const component) {
     //_dbg("onPrinterKilled %s", msg);
     taskENTER_CRITICAL(); //never exit CRITICAL, wanted to use __disable_irq, but it does not work. i do not know why
     HAL_IWDG_Refresh(&hiwdg);
-    marlin_server.vars.device_state = DEVICE_STATE_ERROR;
-    marlin_server.device_state_change = 1;
+    _device_state_change(DEVICE_STATE_ERROR);
     if (_is_thermal_error(msg)) { //todo remove me after new thermal manager
         const marlin_vars_t &vars = marlin_server.vars;
         temp_error(msg, component, vars.temp_nozzle, vars.target_nozzle, vars.temp_bed, vars.target_bed);
--- src/wui/wui.c
+++ src/wui/wui.c
@@ -18,7 +18,7 @@
 #define MAX_WUI_REQUEST_LEN       100
 #define MAX_MARLIN_REQUEST_LEN    100
 #define WUI_FLG_PEND_REQ          0x0001
-#define BUDDY_DISABLE_HTTP_CLIENT //disabling http client for next public release
+//#define BUDDY_DISABLE_HTTP_CLIENT //disabling http client for next public release
 
 osMessageQId tcpclient_wui_queue = 0; // char input queue (uint8_t)
 osSemaphoreId tcpclient_wui_sema = 0; // semaphore handle
