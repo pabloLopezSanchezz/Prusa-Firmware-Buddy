127c54404bbfea043aa6c339448a0dbe5b8d1776
Alan Dragomirecký <alan@dragomirecky.com>
2020-03-19 10:35:15 +0100

    mk4: drop heatbreak fan speed to 80

--- src/common/hwio_a3ides_2209_02.c
+++ src/common/hwio_a3ides_2209_02.c
@@ -684,7 +684,7 @@ void hwio_arduino_digitalWrite(uint32_t ulPin, uint32_t ulVal) {
             //hwio_fan_set_pwm(_FAN1, ulVal?255:0);
             //_hwio_pwm_analogWrite_set_val(HWIO_PWM_FAN1, ulVal ? _pwm_analogWrite_max[HWIO_PWM_FAN1] : 0);
 #ifdef PRINTER_PRUSA_MK4
-            _hwio_pwm_analogWrite_set_val(HWIO_PWM_FAN1, ulVal ? 120 : 0);
+            _hwio_pwm_analogWrite_set_val(HWIO_PWM_FAN1, ulVal ? 80 : 0);
 #else
             _hwio_pwm_analogWrite_set_val(HWIO_PWM_FAN1, ulVal ? 100 : 0);
 #endif
