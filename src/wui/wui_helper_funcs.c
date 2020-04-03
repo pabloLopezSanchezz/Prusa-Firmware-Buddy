f7d034281c0d278d411c61e995c48f7fce6174ed
joshymjose <joshymjose@gmail.com>
2020-04-03 18:00:00 +0200

    check before filling message

--- src/wui/wui_helper_funcs.c
+++ src/wui/wui_helper_funcs.c
@@ -38,12 +38,13 @@ void send_request_to_wui(const char *request) {
     if (0 != tcp_wui_queue_id) // queue valid
     {
         uint32_t q_space = osMessageAvailableSpace(tcp_wui_queue_id);
-
-        wui_cmd_t *mptr;
-        mptr = osPoolAlloc(tcp_wui_mpool_id);
-        strlcpy(mptr->gcode_cmd, request, 100);
-        osMessagePut(tcp_wui_queue_id, (uint32_t)mptr, osWaitForever); // Send Message
-        osDelay(100);
+        if (q_space) {
+            wui_cmd_t *mptr;
+            mptr = osPoolAlloc(tcp_wui_mpool_id);
+            strlcpy(mptr->gcode_cmd, request, 100);
+            osMessagePut(tcp_wui_queue_id, (uint32_t)mptr, osWaitForever); // Send Message
+            osDelay(100);
+        }
     }
     osSemaphoreRelease(tcp_wui_semaphore_id);
 }
