d869c5b71a54e20e0b5d0522f488252ba989ac2c
Alan Dragomirecký <alan@dragomirecky.com>
2020-05-07 18:48:47 +0200

    mk4: make Z_CLEARANCE_BETWEEN_PROBES 0.5 mm

--- include/marlin/Configuration_A3ides_2209_MK4.h
+++ include/marlin/Configuration_A3ides_2209_MK4.h
@@ -933,8 +933,8 @@
  *     But: `M851 Z+1` with a CLEARANCE of 2  =>  2mm from bed to nozzle.
  */
 #define Z_CLEARANCE_DEPLOY_PROBE 0 // Z Clearance for Deploy/Stow
-#define Z_CLEARANCE_BETWEEN_PROBES 1 // Z Clearance between probe points 1
-#define Z_CLEARANCE_MULTI_PROBE 0.7 // Z Clearance between multiple probes
+#define Z_CLEARANCE_BETWEEN_PROBES 0.5 // Z Clearance between probe points 1
+#define Z_CLEARANCE_MULTI_PROBE 0.5 // Z Clearance between multiple probes
 #define Z_AFTER_PROBING 2 // Z position after probing is done 2
 
 #define Z_PROBE_LOW_POINT -2 // Farthest distance below the trigger-point to go before stopping
