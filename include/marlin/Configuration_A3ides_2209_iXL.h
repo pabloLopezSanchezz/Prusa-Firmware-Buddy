884088bc36d2bf119f71483aa73b2f99cda3d532
Marek Bel <marek.bel@posta.cz>
2020-06-03 16:20:00 +0200

    iX: Increase Z_MAX_POS to 195. Set Y_MIN_POS to -3.

--- include/marlin/Configuration_A3ides_2209_iXL.h
+++ include/marlin/Configuration_A3ides_2209_iXL.h
@@ -1016,11 +1016,11 @@
 
 // Travel limits (mm) after homing, corresponding to endstop positions. defaul x -2.5 y -7.3
 #define X_MIN_POS -2
-#define Y_MIN_POS -7
+#define Y_MIN_POS -3
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 180
+#define Z_MAX_POS 195
 
 /**
  * Software Endstops
